2a098c397de31d8cd5a8b6a8c2d31af1fd164fc5
Michael James <mikejlyh@gmail.com>
2019-05-08 21:00:51 +0100

    stripe integration in client

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -9,6 +9,7 @@ import SickButton from "./styles/SickButton";
 import CartItem from "./CartItem";
 import calcTotalPrice from "../lib/calcTotalPrice";
 import formatMoney from "../lib/formatMoney";
+import TakeMyMoney from "./TakeMyMoney";
 
 const LOCAL_STATE_QUERY = gql`
   query {
@@ -50,7 +51,9 @@ const Cart = () => (
 
                   <footer>
                     <p>{formatMoney(calcTotalPrice(me.cart))}</p>
-                    <SickButton>Checkout</SickButton>
+                    <TakeMyMoney>
+                      <SickButton>Checkout</SickButton>
+                    </TakeMyMoney>
                   </footer>
                 </CartStyle>
               )}
