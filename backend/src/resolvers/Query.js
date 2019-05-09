f9d47638a196339372a38ed2da88b6d1d3101d2a
Michael James <mikejlyh@gmail.com>
2019-05-09 22:23:22 +0100

    Order and Orders pages

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -26,12 +26,49 @@ const Query = {
     hasPermission(ctx.request.user, ["ADMIN", "PERMISSIONUPDATE"]);
     // 3. If they do, query all the users
     return ctx.db.query.users({}, info);
-  }
+  },
   // async items(parent, args, ctx, info) {
   //   console.log('Getting Items!!');
   //   const items = await ctx.db.query.items();
   //   return items;
   // },
+  async order(parent, args, ctx, info) {
+    // 1. Make sure they are logged in
+    if (!ctx.request.userId) {
+      throw new Error("You arent logged in!");
+    }
+    // 2. Query the current order
+    const order = await ctx.db.query.order(
+      {
+        where: { id: args.id }
+      },
+      info
+    );
+    // 3. Check if they have the permissions to see this order
+    const ownsOrder = order.user.id === ctx.request.userId;
+    const hasPermissionToSeeOrder = ctx.request.user.permissions.includes(
+      "ADMIN"
+    );
+    if (!ownsOrder || !hasPermissionToSeeOrder) {
+      throw new Error("You cant see this bud");
+    }
+    // 4. Return the order
+    return order;
+  },
+  async orders(parent, args, ctx, info) {
+    const { userId } = ctx.request;
+    if (!ctx.request.userId) {
+      throw new Error("You arent logged in!");
+    }
+    return ctx.db.query.orders(
+      {
+        where: {
+          user: { id: userId }
+        }
+      },
+      info
+    );
+  }
 };
 
 module.exports = Query;
